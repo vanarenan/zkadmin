92a8ede381ab15897c2f3be28495fece6c580791
vbotezat <vbotezat@lab-code.com>
2021-05-05 11:00:00 +0300

    some impruvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'zkadmin';
+  
+  ourDate;
+  
+  showDate(): void {
+    this.ourDate = new Date();
+  }
+  
+  clearDate(): void {
+    this.ourDate = '';
+  }
+  
 }
