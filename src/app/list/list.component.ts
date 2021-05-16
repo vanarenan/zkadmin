e9c4b6da86baf6db9be54cda473e2c7540f176ba
vbotezat <vbotezat@lab-code.com>
2021-05-16 19:49:37 +0300

    123

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -7,39 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['id', 'name', 'photo'];
 
   myDataArray: {}[] = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-    {position: 11, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 12, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 13, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 14, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 15, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 16, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 17, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 18, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 19, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 20, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-    {position: 21, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 22, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 23, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 24, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 25, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 26, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 27, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 28, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 29, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 30, name: 'Neon', weight: 20.1797, symbol: 'Ne'}
+      { id: 1, name: 'Hydrogen', photo: '' },
+      { id: 2, name: 'Helium', photo: '' },
+      { id: 3, name: 'Lithium', photo: '' },
+      { id: 4, name: 'Beryllium', photo: '' },
+      { id: 5, name: 'Boron', photo: '' },
+      { id: 6, name: 'Carbon', photo: '' },
+      { id: 7, name: 'Nitrogen', photo: '' },
+      { id: 8, name: 'Oxygen', photo: '' },
+      { id: 9, name: 'Fluorine', photo: '' },
+      { id: 10, name: 'Neon', photo: '' },
+      { id: 11, name: 'Hydrogen', photo: '' },
+      { id: 12, name: 'Helium', photo: '' },
+      { id: 13, name: 'Lithium', photo: '' },
+      { id: 14, name: 'Beryllium', photo: '' },
+      { id: 15, name: 'Boron', photo: '' },
+      { id: 16, name: 'Carbon', photo: '' },
+      { id: 17, name: 'Nitrogen', photo: '' },
+      { id: 18, name: 'Oxygen', photo: '' },
+      { id: 19, name: 'Fluorine', photo: '' },
+      { id: 20, name: 'Neon', photo: '' }
   ];
 
   constructor() { }
