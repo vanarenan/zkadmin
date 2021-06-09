1db57556ec0bb3db582783a175ced9afd900c7d8
vbotezat <vbotezat@deloitte.com>
2021-06-09 04:46:22 +0300

    Added form, installed npm modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import * as moment from 'moment';
+
+import { MAT_DATE_LOCALE, MAT_DATE_FORMATS } from '@angular/material/core';
+import { MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatMomentDateModule, MAT_MOMENT_DATE_FORMATS } from '@angular/material-moment-adapter';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatTableModule } from '@angular/material/table';
-import { MatCardModule } from '@angular/material/card';
 
 import { ListComponent } from './list/list.component';
 import { UserComponent } from './user/user.component';
@@ -19,12 +27,23 @@ import { UserComponent } from './user/user.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatCardModule,
     MatTableModule,
-    MatCardModule
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatMomentDateModule
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline', floatLabel: 'always' } },
+    { provide: MAT_DATE_LOCALE, useValue: 'uk' },
+    { provide: MAT_DATE_FORMATS, useValue: { display: { dateInput: 'DD/MM/YYYY' } } }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit {
+export class UserComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  userForm = new FormGroup({
+    fullName: new FormControl('', [Validators.required]),
+    gender: new FormControl(''),
+    dateofbirth: new FormControl(''),
+    phone: new FormControl(''),
+    email: new FormControl('', [Validators.email]),
+    department: new FormControl(''),
+    position: new FormControl('')
+  });
 
 }
